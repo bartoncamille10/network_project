fe6f8e95d76fa6bc91029107bbabf0c502014732
joshualemecool <joshua.lucas.jl2@gmail.com>
2025-02-05 15:02:35 +0100

    changement api privee

--- api_pub.py
+++ api_pub.py
@@ -1,11 +1,42 @@
-import flask
+from flask import Flask, jsonify, request
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app = Flask(__name__)
 
+@app.route("/account/<int:account>/exists", methods=["GET"])
+def account_exists(account):
+    # test SANS interroger de base de données
+    if account == 708273:
+        return jsonify({}), 200  # Le compte existe
+    return jsonify({"error": "Le compte n'existe pas."}), 404
 
-@app.route('/', methods=['GET'])
-def home():
-   return "<h1>Annuaire Internet</h1><p>Ce site est le prototype d’une API mettant à disposition des données sur les employés d’une entreprise.</p>"
+@app.route("/currency/<string:currency>/allowed", methods=["GET"])
+def currency_allowed(currency):
+    return jsonify({"error": "Not Implemented Yet"}), 501
 
-app.run()
+@app.route("/currency/<string:currency>/rate", methods=["POST"])
+def set_currency_rate(currency):
+    return jsonify({"error": "Not Implemented Yet"}), 501
+
+@app.route("/transaction/card", methods=["POST"])
+def transaction_card():
+    data = request.get_json()
+    if not data or "sourceAccount" not in data or "amount" not in data:
+        return jsonify({"error": "Paramètres invalides"}), 400
+    return jsonify({}), 200  # Transaction acceptée (simulation)
+
+@app.route("/transaction/check", methods=["POST"])
+def transaction_check():
+    data = request.get_json()
+    if not data or "sourceAccount" not in data or "amount" not in data:
+        return jsonify({"error": "Paramètres invalides"}), 400
+    return jsonify({}), 200  # Simulation d'enregistrement
+
+@app.route("/transaction/transfer", methods=["POST"])
+def transaction_transfer():
+    data = request.get_json()
+    if not data or "sourceAccount" not in data or "amount" not in data:
+        return jsonify({"error": "Paramètres invalides"}), 400
+    return jsonify({}), 200  # Simulation d'enregistrement
+
+if __name__ == "__main__":
+    app.run(host="10.0.2.1", port=5000, debug=True)
